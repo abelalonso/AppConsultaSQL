48ac0404f813712283fcc281d51b2f891371fa72
Abel <patriciacarrasco@Patricias-iMac.local>
2018-02-03 12:30:15 +0000

    Añadimos la interfaz gráfica

---
+++ src/AppConsulta.java
@@ -0,0 +1,45 @@
+
+import java.awt.*;
+import javax.swing.*;
+
+public class AppConsulta {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		JFrame marco=new MarcoAplicacion();
+		marco.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		marco.setVisible(true);
+	}
+}
+
+class MarcoAplicacion extends JFrame{
+	
+	public MarcoAplicacion(){
+		setTitle("Consulta BBDD");
+		setBounds(500, 300, 400, 400);
+		setLayout(new BorderLayout());
+		JPanel menus=new JPanel();
+		menus.setLayout(new FlowLayout());
+		secciones=new JComboBox();
+		secciones.setEditable(false);
+		secciones.addItem("Todos");
+		paises=new JComboBox();
+		paises.setEditable(false);
+		paises.addItem("Todos");
+		resultado=new JTextArea(4, 50);
+		resultado.setEditable(false);
+		add(resultado);
+		menus.add(secciones);
+		menus.add(paises);
+		add(menus, BorderLayout.NORTH);
+		add(resultado, BorderLayout.CENTER);
+		JButton botonConsulta=new JButton("Consulta");
+		add(botonConsulta, BorderLayout.SOUTH);
+	}
+	
+	private JComboBox secciones;
+	private JComboBox paises;
+	private JTextArea resultado;
+	
+}
+
